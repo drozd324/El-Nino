890de37deb8302a0ad3ba7d71e138b38c38f4260
Patryk Drozd <68166254+drozd324@users.noreply.github.com>
2023-04-05 20:06:01 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ def system(state, t):
 
     return du, dTe, dTw
 
-#this is the final modified system with u**=u*(1+3sin(2pit)
+#this is the final modified system with u*=u*(1+3sin(2pit))
 def mod_system(state, t):
     u, T_e, T_w = state
 
@@ -40,7 +40,7 @@ def mod_system(state, t):
     delta_x = 7.5 # 7.5
 
     # setting up the 3 differential equations
-    du = B / delta_x * (T_e - T_w) - C * (u - u_star*(1+3*np.sin(2*np.pi*t))) #  = du/dt
+    du = B / delta_x * (T_e - T_w) - C * (u - u_star * ( 1 + 3 * np.sin( 2 * np.pi * t ))) #  = du/dt
     dTe = u * T_w / (2 * delta_x) - A * (T_e - T_star) #  = dTe/dt
     dTw = -u * T_e / (2 * delta_x) - A * (T_w - T_star) #  = dTw/dt
 
@@ -55,7 +55,8 @@ def diff(func, x, dx):
 
 # this will turn the numpy array into a something that works like a mathematical function
 def make_func(list_func, x):
-    if len(list_func) - 1 < int(round(x/dt)): # this bit makes sure that the 
+    # first bit makes sure that we wont round into an index outside the reach of the list
+    if len(list_func) - 1 < int(round(x/dt)):
         return list_func[-1]
     else:
         return list_func[int(round(x/dt))]
@@ -145,17 +146,19 @@ def standard_dev(mylist):
 
 dt = .01 # resolution
 t_start = 0 # this will allways be 0
-t_end = 200 # the maximum value of time 
+t_end = 1000 # the maximum value of time 
 
 t = np.arange(t_start, t_end, dt)
 state_0 = [10, 10, 14] # initial conditions
 y = odeint(system, state_0, t) # y is a list with elemets that are list with 3 entries containing u, Te, Tw for each time step
-mod_y = odeint(mod_system, state_0, t)
 
 # this corresponds to the functions u, Te, Tw which are function of time
 u = y[:,0]
 Te = y[:,1]
 Tw = y[:,2]
+
+# calculating u, Te, Tw for modified system
+mod_y = odeint(mod_system, state_0, t)
 
 mod_u = mod_y[:,0]
 mod_Te = mod_y[:,1]
@@ -163,8 +166,9 @@ mod_Tw = mod_y[:,2]
 
 # first plots
 
-plt.plot(t, u, label="u")
-#plt.plot(t, mod_u, label="mod_u")
+
+"""plt.plot(t, u, label="u")
+plt.plot(t, mod_u, label="mod_u")
 plt.title("Current velocity against time ({} years)".format(t_end))
 plt.xlabel("Time t (years)")
 plt.ylabel("Current Velocity u ") # 10^3km / year  units?
@@ -172,7 +176,8 @@ plt.ylim((-400,400))
 plt.xlim((0, t_end))
 plt.legend()
 plt.grid(axis = 'y')
-#plt.show()
+plt.show()
+"""
 
 """
 plt.plot(t, Te - Tw)
@@ -205,6 +210,7 @@ print("The number of ENSO events in {} years is: {}".format(t_end, num_of_elNino
 times_between_ENSO = [] # periods of time
 for i in range(num_of_elNinos - 1):
     times_between_ENSO.append(roots[i+1] - roots[i]) # roots is sorted so dont need abs() function
+times_between_ENSO.sort()
 
 # finding the mean, and standard deviations (igonring the first 10 ENSO events)
 average_T = mean(times_between_ENSO[10:])
@@ -213,8 +219,9 @@ print("The mean time between ENSO events (ignoring the first 10) is: {} years".f
 print("The standard deviation of ENSO events (ignoring the first 10) is: {}".format(dev_T))
 
 # this will mark all the maxima in the list of roots on the graph
-for i in range(len(roots)):
+"""for i in range(len(roots)):
     plt.scatter(roots[i], make_func(u, roots[i]))
+plt.show() """
 
 # histogram plot make nicer
 """plt.hist(times_between_ENSO, bins= 30)
@@ -231,24 +238,24 @@ plt.ylabel("T_e - T_w ")
 plt.ylim((-30,30))
 plt.show()"""
 
-"""# plot of ti against ti+10, ignoring the first 10 events
-plt.plot(times_between_ENSO[10:-10], times_between_ENSO[20:])
+# i have a feeling this is wrong (idk)
+# plot of ti against ti+10, ignoring the first 10 events ( maybe not necessary i think we should start at 5)
+"""plt.plot(times_between_ENSO[10:-10], times_between_ENSO[20:])
 plt.title("Ti vs T(i+10)")
 plt.xlabel("T(i+10)")
 plt.ylabel("Ti")
-plt.show()
+plt.show()"""
 # correlation = stairs
-# if uncorrelated, graph would have no distinguishable pattern"""
+# if uncorrelated, graph would have no distinguishable pattern
 
 # 
-ENSO_months = []
+"""ENSO_months = []
 for i in range(len(mod_roots)):
-    ENSO_months.append(mod_roots[i]%1)
+    ENSO_months.append(mod_roots[i]%1)"""
     
 # histogram plot for amount of 
-plt.hist(ENSO_months, bins= 12)
+"""plt.hist(ENSO_months, bins= 12)
 plt.ylabel("ENSO event")
 plt.xlabel("Months")
-plt.show()
+plt.show()"""
 
-plt.show() 
